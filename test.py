e02b3afd68e68686d9c579236549934e2d333add
yongbin <ybfeng94@gmail.com>
2022-10-24 22:06:53 -0400

    add argparse

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 from modules.loganalyzer import process_file, print_info
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--log', help='foo help')
+parser.add_argument('--log2', help='Path to output log2 file', default=None)
+args = parser.parse_args()
+
 
 # process_file will return a collection of results in that log file, each model with their counts and latency.
-results_1st = process_file("./testdata/result1.log")
-results_2nd = process_file("./testdata/result2.log")
+results_1st = process_file(args.log)
 
-# print out the information of the 1st log file
-#print_info(results_1st)
+if args.log2:
+    results_2nd = process_file(args.log2)
+    # print out the information between 1st and 2nd log file
+    print_info(results_1st, results_2nd)
 
-# print out the information between 1st log and 2nd log
-print_info(results_1st, results_2nd)
+else:
+    # print out the information of the 1st log file
+    print_info(results_1st)
